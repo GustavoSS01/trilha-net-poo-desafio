b06dffebf991a022abb3e6cf58c113985a03d6df
GustavoSS01 <gustavodssilva456@gmail.com>
2024-01-02 16:01:25 -0300

    pequena alteração em Program.cs

--- Program.cs
+++ Program.cs
@@ -3,13 +3,13 @@
 // Implementado - Realizar os testes com as classes Nokia e Iphone
 
 Console.WriteLine("Smartphone Iphone");
-Smartphone iphone = new Iphone(numero:"12341", modelo:"iPhone 4", imei:"121212121", memoria:128);
+Smartphone iphone = new Iphone(numero:"12341", modelo:"iPhone 4", imei:"121212121", memoria:32);
 iphone.ReceberLigacao();
 iphone.InstalarAplicativo("Spotify");
 
 Console.WriteLine("\n");
 
 Console.WriteLine("Smartphone Nokia");
-Smartphone nokia = new Nokia(numero:"67896", modelo:"Nokia Lumia 800", imei:"343434343", memoria:64);
+Smartphone nokia = new Nokia(numero:"67896", modelo:"Nokia Lumia 800", imei:"343434343", memoria:16);
 nokia.ReceberLigacao();
 nokia.InstalarAplicativo("Youtube");
