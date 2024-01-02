46bb594a525babf0b590d7bb26796f1d893620f8
GustavoSS01 <gustavodssilva456@gmail.com>
2024-01-02 15:54:51 -0300

    conclusão do desafio de código POO da DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Implementado - Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero:"12341", modelo:"iPhone 4", imei:"121212121", memoria:128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Spotify");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero:"67896", modelo:"Nokia Lumia 800", imei:"343434343", memoria:64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Youtube");
